8f3da0fdc9bd6f78b0569550570966d0cf52b42d
akilawickey <asak999@gmail.com>
2017-05-19 09:39:34 +0000

    for sure case

--- rest-api/test/test.js
+++ rest-api/test/test.js
@@ -4,6 +4,9 @@ app.listen(1212);
 app.get('/', function (req,res) {
   console.log('connected');
 })
+
+
+
 app.get('/sensor/:temp/:hmdt', function(req, res) {
      var t = req.params.temp;
      var h = req.params.hmdt;
